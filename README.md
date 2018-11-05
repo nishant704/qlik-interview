e8c22c950170a5443c34437abbb9b310c0cbbb63
Nishant Singh <nishant2.singh@paytm.com>
2018-11-05 13:02:03 +0530

    Added Readme , TF

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # qlik-interview
 Contains Application along with DockerFile and Terraform
+
+1. Terraform Version : Terraform v0.11.9
+2. Ansible Version : Ansible 2.4.0.0
--- terraform/global/D-EC2-Initial/A-Create-Jumpbox-secgroup.tf
+++ terraform/global/D-EC2-Initial/A-Create-Jumpbox-secgroup.tf
@@ -17,6 +17,13 @@ resource "aws_security_group" "app-sec-group" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = "80"
+    to_port     = "80"
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
